d4089d392b1887809fbec086621fa12d30b7ee01
Angelina <75496111+Angelina207@users.noreply.github.com>
2022-07-05 14:45:02 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as scriptForm from "./modules/form.js";
+import * as smoothScrll from "./modules/smooth-scroll.js";
 
 flsFunctions.isWebp();
 scriptForm.form();
+smoothScrll.scroll(); 
 
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
---
+++ src/js/modules/smooth-scroll.js
@@ -0,0 +1,14 @@
+export function scroll() {
+    const linkScroll = document.querySelectorAll('a[href^="#"]');
+    for (let link of linkScroll) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const id = link.getAttribute('href');
+
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    }
+}
