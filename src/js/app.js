fde5b29d9f618def0a98166e64aee843f9113262
Angelina <75496111+Angelina207@users.noreply.github.com>
2022-07-06 10:32:59 +0300

    module

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,12 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as scriptForm from "./modules/form.js";
 import * as smoothScrll from "./modules/smooth-scroll.js";
+import * as burdegMenu from "./modules/burger.js";
 
 flsFunctions.isWebp();
 scriptForm.form();
 smoothScrll.scroll(); 
-
+burdegMenu.burger();
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
