0c8e5f7f86e7f09cd94b73ef4d949153f5faf1ac
Angelina <75496111+Angelina207@users.noreply.github.com>
2022-07-07 11:45:41 +0300

    commit

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -2,11 +2,14 @@ export function burger() {
     const burgerBtnOpen = document.querySelector('.header__burger').addEventListener('click', navOpen);
     const burgerBtnClose = document.querySelector('.nav-btn-inner').addEventListener('click', navClose);
     const navPage = document.querySelector('.header__nav');
+    const overlay = document.querySelector('.overlay');
     
     function navOpen() {
         navPage.classList.add('is-open');
+        overlay.style.display = 'block';
     }
     function navClose() {
         navPage.classList.remove('is-open');
+        overlay.style.display = 'none';
     }
 }
